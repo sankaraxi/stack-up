74b0b062dc54f4b4982925725e480e48af8a47fa
Sankar K G <sankarfrompalani@gmail.com>
2025-03-22 15:33:21 +0530

    add database connection and user model with signup endpoint

--- server/index.js
+++ server/index.js
@@ -1,24 +1,27 @@
 const express = require('express');
-
+const connectDB = require('./src/config/database');
+const User = require('./src/models/user');
 const app = express();
 
-app.use("/test/second",(req, res) => {
-    res.send('Hello World from my second official server!');
-}) 
+app.post('/signup', (req, res) => {
+    const user = new User({
+        firstName: "MS",
+        lastName: "Dhoni",
+        email: "msdhoni.csk@gmail.com",
+        password: "sakshi",
+        age: 40,
+        gender: "Male"
+    })
 
-app.use("/test",(req, res) => {
-    res.send('Hello World from my first official server!');
+    user.save();
+    res.send("User created successfully");
 })
 
-app.get('/middle', (req, res) => {
-    
-    res.send('Hello World from my first official server!');
-})
-app.get('/user', (req, res) => {
-    res.send({firstname: 'Sankar', lastname: 'Gnanasekar'});
-});
-
-
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+connectDB().then(() => {
+    console.log('Database connected');
+    app.listen(5000, () => {
+        console.log('Server is running on port 5000');
+    });
+}).catch((err) => {
+    console.log('Database connection failed');
 });
